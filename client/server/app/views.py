35a1da11a97496ff91645ef8bdab684719ffd4ea
olagodavid <olagodavid@gmail.com>
2016-09-30 15:08:19 +0300

    views file

---
+++ client/server/app/views.py
@@ -0,0 +1,28 @@
+from flask import g
+from flask.ext import restful
+
+from server import api, db, flask_bcrypt, auth
+from models import User, Post
+from forms import UserCreateForm, SessionCreateForm, PostCreateForm
+from serializers import UserSerializer, PostSerializer
+
+
+@auth.verify_password
+def verify_password(email, password):
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return False
+    g.user = user
+    return flask_bcrypt.check_password_hash(user.password, password)
+
+
+class UserView(restful.Resource):
+    def post(self):
+        form = UserCreateForm()
+        if not form.validate_on_submit():
+            return form.errors, 422
+
+        user = User(form.email.data, form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return UserSerializer(user).data
