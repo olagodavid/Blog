4aeac67cbaeaedd88133df8a19d48da4f3492436
olagodavid <olagodavid@gmail.com>
2016-09-30 15:12:46 +0300

    views file

--- client/server/app/views.py
+++ client/server/app/views.py
@@ -27,38 +27,42 @@ class UserView(restful.Resource):
         db.session.commit()
         return UserSerializer(user).data
 
-    class SessionView(restful.Resource):
-        def post(self):
-            form = SessionCreateForm()
-            if not form.validate_on_submit():
-                return form.errors, 422
 
-            user = User.query.filter_by(email=form.email.data).first()
-            if user and flask_bcrypt.check_password_hash(user.password, form.password.data):
-                return UserSerializer(user).data, 201
-            return '', 401
+class SessionView(restful.Resource):
+    def post(self):
+        form = SessionCreateForm()
+        if not form.validate_on_submit():
+            return form.errors, 422
 
-    class PostListView(restful.Resource):
-        def get(self):
-            posts = Post.query.all()
-            return PostSerializer(posts, many=True).data
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and flask_bcrypt.check_password_hash(user.password, form.password.data):
+            return UserSerializer(user).data, 201
+        return '', 401
 
-        @auth.login_required
-        def post(self):
-            form = PostCreateForm()
-            if not form.validate_on_submit():
-                return form.errors, 422
-            post = Post(form.title.data, form.body.data)
-            db.session.add(post)
-            db.session.commit()
-            return PostSerializer(post).data, 201
 
-    class PostView(restful.Resource):
-        def get(self, id):
-            posts = Post.query.filter_by(id=id).first()
-            return PostSerializer(posts).data
+class PostListView(restful.Resource):
+    def get(self):
+        posts = Post.query.all()
+        return PostSerializer(posts, many=True).data
 
-    api.add_resource(UserView, '/api/v1/users')
-    api.add_resource(SessionView, '/api/v1/sessions')
-    api.add_resource(PostListView, '/api/v1/posts')
-    api.add_resource(PostView, '/api/v1/posts/<int:id>')
+    @auth.login_required
+    def post(self):
+        form = PostCreateForm()
+        if not form.validate_on_submit():
+            return form.errors, 422
+        post = Post(form.title.data, form.body.data)
+        db.session.add(post)
+        db.session.commit()
+        return PostSerializer(post).data, 201
+
+
+class PostView(restful.Resource):
+    def get(self, id):
+        posts = Post.query.filter_by(id=id).first()
+        return PostSerializer(posts).data
+
+
+api.add_resource(UserView, '/api/v1/users')
+api.add_resource(SessionView, '/api/v1/sessions')
+api.add_resource(PostListView, '/api/v1/posts')
+api.add_resource(PostView, '/api/v1/posts/<int:id>')
